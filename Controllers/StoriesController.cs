2554b95ebf12a1dd8b69c4d577eca1ccaed8e67b
LuisGonzaga <luis.caldas.costa@gmail.com>
2019-12-15 19:16:58 +0000

    Project first upload

---
+++ Controllers/StoriesController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+using Newtonsoft.Json;
+
+
+namespace SantanderBestStoriesAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class StoriesController : Controller
+    {
+        private static IMemoryCache _cachedStories;
+        private static readonly HttpClient httpClient = new HttpClient();
+
+        [HttpGet]
+        public ActionResult<object> GetTwentyTopStories()
+        {
+            if (_cachedStories == null) InsertTopTwentyStoriesInCache();
+            return _cachedStories.Get(Stories.TopTwenty);
+        }
+
+        #region Stories CacheOperations
+        private void InsertTopTwentyStoriesInCache()
+        {
+            string StoriesList = RetrieveStories();
+            var StoriesList2Model = JsonConvert.DeserializeObject<List<Story>>(StoriesList.ToString());
+            var TopTwentyStories = StoriesList2Model.Take(20).OrderByDescending(o => o.score).ToList();
+            CreateCachedStoriesList(TopTwentyStories);
+        }
+        private static void CreateCachedStoriesList(List<Story> StoriesList)
+        {
+            var cacheEntryOptions = new MemoryCacheEntryOptions().SetAbsoluteExpiration(TimeSpan.FromDays(1));
+            _cachedStories = new MemoryCache(new MemoryCacheOptions());
+            _cachedStories.Set(Stories.TopTwenty, StoriesList, cacheEntryOptions);
+        }
+
+        #endregion
+
+
+        #region HackerNewsAPI Readings
+        private string RetrieveStories()
+        {            
+            string url = "https://hacker-news.firebaseio.com/v0/beststories.json";
+            var storyIds = httpClient.GetAsync(url).Result.Content.ReadAsStringAsync().Result;
+            var jsonStoriesById = JsonConvert.DeserializeObject<List<int>>(storyIds);
+            string strStoriesDetailedList = "[";
+            foreach (var story in jsonStoriesById)
+            {
+                string storyDetails = StoryDetailsById(story);
+                strStoriesDetailedList += storyDetails + ",";
+            }
+            strStoriesDetailedList = strStoriesDetailedList.TrimEnd(',');
+            strStoriesDetailedList += "]";
+            return strStoriesDetailedList;
+        }
+
+        private string StoryDetailsById(int StoryId)
+        {
+            string url = "https://hacker-news.firebaseio.com/v0/item/" + StoryId + ".json";
+            return httpClient.GetAsync(url).Result.Content.ReadAsStringAsync().Result;
+        }
+
+        #endregion
+    }
+}
---
+++ Models/Stories.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace SantanderBestStoriesAPI
+{
+    public static class Stories
+    {
+        public static string TopTwenty { get { return "_TopTwenty"; } }        
+    }
+
+#pragma warning disable IDE1006 // Naming Styles
+    public class Story
+    {
+        public string by { get; set; }
+        public int descendants { get; set; }
+        public int id { get; set; }
+        public List<int> kids { get; set; }
+        public int score { get; set; }
+        public int time { get; set; }
+        public string title { get; set; }
+        public string type { get; set; }
+        public string url { get; set; }
+    }
+}
